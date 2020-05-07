1c0f615f7acdd6cd0301e5345f0136c252b8112f
Sanjeev Gadre <sanjeev_gadre@hotmail.com>
2020-05-07 16:32:24 +0530

    Code Optimization for get_data.py

--- get_data_1.py
+++ get_data_1.py
@@ -8,6 +8,7 @@ Created on Tue May  5 09:01:57 2020
 
 # %% Libraries
 import numpy as np
+import pandas as pd
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -15,14 +16,14 @@ from bs4 import BeautifulSoup
 # %% Get Data
 '''home = 'https://www.boxofficeindia.com/'
 years = np.arange(2000, 2020, 1)
-adj_net_gross_url = home + 'india-adjusted-nett-gross.php'
+adj_nett_gross_url = home + 'india-adjusted-nett-gross.php'
 
 years = [2000]      # To test subsequent code. To be deleted later
 
 for y in years:
     params = {'year' : y}
     try:
-        year_html = requests.get(adj_net_gross_url, params = params)
+        year_html = requests.get(adj_nett_gross_url, params = params)
         soup = BeautifulSoup(year_html.content, 'html.parser')
         movies = soup.find_all('a', class_ = 'anchormob')   # Finds all 'a' tags with class = 'anchormob'
         for movie in movies:                                # For each movie in that year
@@ -38,169 +39,179 @@ for y in years:
     except ConnectionError:
         continue'''
         
-# %% Step 1
+# %% Step 1: Find the top movies for a given year
 home = 'https://www.boxofficeindia.com/'
 net_gross_url = home + 'india-total-nett-gross.php'
-year = 2000                                                 # For now doing it for only one year
+
+year = 2000                                                         # For now doing it for only one year
 
 param = {'year' : year}
 try:
     year_html = requests.get(net_gross_url, params = param)
-    soup = BeautifulSoup(year_html.content, 'html.parser')
-    movies = soup.find_all('a', class_= 'anchormob')        # Finds the top 50 movies for the year
+    year_soup = BeautifulSoup(year_html.content, 'html.parser')
+    movies = year_soup.find_all('a', class_= 'anchormob')          # Finds the top 50 movies for the year
 except ConnectionError:
     exit 
 
-#%% Change type from bs4.navigable.string to integers. Additionally strip the commas in the string. Used       in Step 2
-def tointeger01(field):
-    field = str(field)
-    field = field.replace(',', '')
-    field = int(field)
-    return field
+#%% Step 2: For a movie from the list formed in Step 1, scrape all the data (except weekly regional numbers). 
 
-#%% Change type from bs4.navigable.string to integers. Additionally strip the commas and | in the string. Used in Step 2
-def tointeger02(field):
+def tointeger(field):
+    '''
+    Change type from bs4.navigable.string to integers. Additionally strip the "," and "|" in the string.
+
+    Parameters
+    ----------
+    field : bs4.navigable.string
+        Numerical data scrapped from the webpage as a string.
+
+    Returns
+    -------
+    field : int
+        Appropriately formatted integer.
+
+    '''
     field = str(field)
     field = field.replace(',', '')
     field = field.replace('|', '')
     field = int(field)
     return field
 
-#%% Step 2
-movie_rank = 0
-for movie in movies[:1]:                                    # For now doing it for only one movie
+def movie_val_in_soup(string):
+    '''
+    Finds the value associated with the string from the movie_soup 
+
+    Parameters
+    ----------
+    string : str
+        A string associated with a specific field.
+
+    Returns
+    -------
+    field : TYPE
+        Appropriately formatted integer.
+
+    '''
+    
+    field = movie_soup.find('a', href = string).find_next('img', src = re.compile('images/rupeesim-black.png')).next
+    field = tointeger(field)
+    return field
+
+movie_id = 0
+movie_lst = []
+regn_cons_lst = []
+regn_week_lst = []
+
+for movie in movies[:1]:                                           # For now doing it for only one movie    
     movie_href = movie.get('href')
     movie_url = home + movie_href
     movie_html = requests.get(movie_url)
-    soup = BeautifulSoup(movie_html.content, 'html.parser')
-    movie_rank += 1 
+    movie_soup = BeautifulSoup(movie_html.content, 'html.parser')
     
-    key = int(str(year) + str(movie_rank).zfill(2))          # zfill() pads 0s to the left
-
-    title = soup.find('a', href = re.compile('movieid')).text
+    movie_id += 1 
+    
+    title = movie_soup.find('a', href = re.compile('movieid')).text
     title = str(title)
-
-    release_date = soup.find('span', class_ = 'redtext').text
+    
+    release_date = movie_soup.find('span', class_ = 'redtext').text
     release_date = str(release_date)
     
-    runtime = soup.find('a', href = re.compile('running-time.php')).next.next.next
+    runtime = movie_soup.find('a', href = re.compile('running-time.php')).next.next.next
     runtime = str(runtime)
     runtime = runtime.replace('min', '')
     runtime = int(runtime)
     
-    genre = soup.find('a', href = re.compile('genre.php')).text
+    genre = movie_soup.find('a', href = re.compile('genre.php')).text
     genre = str(genre)
     
-    screens = soup.find('a', href = re.compile('screens.php')).find_next('td', class_ = 'td_cst_wd').text
+    screens = movie_soup.find('a', href = re.compile('screens.php')).find_next('td', class_ = 'td_cst_wd').text
     screens = int(str(screens))
     
-    budget = soup.find('a', href = re.compile('budget.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    budget = tointeger01(budget)
+    india_footfalls = movie_soup.find('a', href = re.compile('india-footfalls.php?')).find_next('td').find_next('td').text
+    india_footfalls = tointeger(india_footfalls)
     
-    india_footfalls = soup.find('a', href = re.compile('india-footfalls.php?')).find_next('td').find_next('td').text
-    india_footfalls = tointeger01(india_footfalls)
+    budget = movie_val_in_soup('budget.php')
     
-    india_first_day = soup.find('a', href = re.compile('india-first-day.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_first_day = tointeger01(india_first_day)
+    india_first_day = movie_val_in_soup('india-first-day.php')
     
-    india_first_weekend = soup.find('a', href = re.compile('india-first-weekend.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_first_weekend = tointeger01(india_first_weekend)
+    india_first_weekend = movie_val_in_soup('india-first-weekend.php')
+        
+    india_first_week = movie_val_in_soup('india-first-week.php')
     
-    india_first_week = soup.find('a', href = re.compile('india-first-week.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_first_week = tointeger01(india_first_week)
+    india_nett_gross = movie_val_in_soup('net_box_office.php')
     
-    india_nett_gross = soup.find('a', href = re.compile('net_box_office.php?')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_nett_gross = tointeger01(india_nett_gross)
+    india_gross = movie_val_in_soup('india-total-gross.php')
     
-    india_gross = soup.find('a', href = re.compile('india-total-gross.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_gross = tointeger01(india_gross)
+    india_disti_nett_gross = movie_val_in_soup('india-distributor-share.php')
     
-    india_adj_net_gross = soup.find('a', href = re.compile('india-adjusted-nett-gross.php?')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_adj_net_gross = tointeger01(india_adj_net_gross)
+    india_adj_nett_gross = movie_val_in_soup('india-adjusted-nett-gross.php?fm=1')
     
-    world_gross = soup.find('a', href = re.compile('worldwide-total-gross.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    world_gross = tointeger01(world_gross)
+    world_gross = movie_val_in_soup('worldwide-total-gross.php')
     
-    india_disti_net_gross = soup.find('a', href = re.compile('india-distributor-share.php')).find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    india_disti_net_gross = tointeger01(india_disti_net_gross)
+    rec = [movie_id, title, release_date, runtime, genre, screens, india_footfalls, 
+           budget, world_gross, india_gross, india_nett_gross, india_disti_nett_gross, india_adj_nett_gross, 
+           india_first_day, india_first_weekend, india_first_week]
+    movie_lst.append(rec)
+        
+    # Territory Consolidated Revenue Data
+    mumbai_nett_gross = movie_val_in_soup('net_box_office.php?cityName=mumbai')
     
-    mumbai_net_gross = soup.find('a', href = 'net_box_office.php?cityName=mumbai').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    mumbai_net_gross = tointeger02(mumbai_net_gross)
-    
-    mumbai_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=mumbai').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    mumbai_disti_net_gross = tointeger01(mumbai_disti_net_gross)
-    
-    delhi_net_gross = soup.find('a', href = 'net_box_office.php?cityName=delhi_up').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    delhi_net_gross = tointeger02(delhi_net_gross)
-    
-    delhi_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=delhi_up').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    delhi_disti_net_gross = tointeger01(delhi_disti_net_gross)
-     
-    epunjab_net_gross = soup.find('a', href = 'net_box_office.php?cityName=east_punjab').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    epunjab_net_gross = tointeger02(epunjab_net_gross)
-    
-    epunjab_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=east_punjab').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    epunjab_disti_net_gross = tointeger01(epunjab_disti_net_gross)
-     
-    rajasthan_net_gross = soup.find('a', href = 'net_box_office.php?cityName=rajasthan').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    rajasthan_net_gross = tointeger02(rajasthan_net_gross)
-    
-    rajasthan_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=rajasthan').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    rajasthan_disti_net_gross = tointeger01(rajasthan_disti_net_gross)
-    
-    cpberar_net_gross = soup.find('a', href = 'net_box_office.php?cityName=cp_berar').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    cpberar_net_gross = tointeger02(cpberar_net_gross)
-    
-    cpberar_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=cp_berar').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    cpberar_disti_net_gross = tointeger01(cpberar_disti_net_gross)
-    
-    ci_net_gross = soup.find('a', href = 'net_box_office.php?cityName=ci').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    ci_net_gross = tointeger02(ci_net_gross)
-    
-    ci_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=ci').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    ci_disti_net_gross = tointeger01(ci_disti_net_gross)
-    
-    nizam_net_gross = soup.find('a', href = 'net_box_office.php?cityName=nizam').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    nizam_net_gross = tointeger02(nizam_net_gross)
-    
-    nizam_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=nizam').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    nizam_disti_net_gross = tointeger01(nizam_disti_net_gross)
-    
-    mysore_net_gross = soup.find('a', href = 'net_box_office.php?cityName=mysore').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    mysore_net_gross = tointeger02(mysore_net_gross)
-    
-    mysore_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=mysore').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    mysore_disti_net_gross = tointeger01(mysore_disti_net_gross)
-    
-    tnkerala_net_gross = soup.find('a', href = 'net_box_office.php?cityName=tn_kerla').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    tnkerala_net_gross = tointeger02(tnkerala_net_gross)
-    
-    tnkerala_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=tn_kerla').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    tnkerala_disti_net_gross = tointeger01(tnkerala_disti_net_gross)
-    
-    bihar_net_gross = soup.find('a', href = 'net_box_office.php?cityName=bihar').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    bihar_net_gross = tointeger02(bihar_net_gross)
-    
-    bihar_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=bihar').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    bihar_disti_net_gross = tointeger01(bihar_disti_net_gross)
-    
-    wb_net_gross = soup.find('a', href = 'net_box_office.php?cityName=west_bengal').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    wb_net_gross = tointeger02(wb_net_gross)
-    
-    wb_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=west_bengal').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    wb_disti_net_gross = tointeger01(wb_disti_net_gross)
-    
-    assam_net_gross = soup.find('a', href = 'net_box_office.php?cityName=assam').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    assam_net_gross = tointeger02(assam_net_gross)
-    
-    assam_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=assam').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    assam_disti_net_gross = tointeger01(assam_disti_net_gross)
-    
-    orrisa_net_gross = soup.find('a', href = 'net_box_office.php?cityName=orrisa').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    orrisa_net_gross = tointeger02(orrisa_net_gross)
-    
-    orrisa_disti_net_gross = soup.find('a', href = 'india-distributor-share.php?cityName=orrisa').find_next('img', src = re.compile('images/rupeesim-black.png')).next
-    orrisa_disti_net_gross = tointeger01(orrisa_disti_net_gross)
+    mumbai_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=mumbai')
 
-#%% Converting bs4 strings to sensible datatypes
-    print(tointeger02(mumbai_net_gross))
+    delhi_nett_gross = movie_val_in_soup('net_box_office.php?cityName=delhi_up')
+    
+    delhi_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=delhi_up')
+
+    epunjab_nett_gross = movie_val_in_soup('net_box_office.php?cityName=east_punjab')
+
+    epunjab_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=east_punjab')
+
+    rajasthan_nett_gross = movie_val_in_soup('net_box_office.php?cityName=rajasthan')
+  
+    rajasthan_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=rajasthan')
+    
+    cpberar_nett_gross = movie_val_in_soup('net_box_office.php?cityName=cp_berar')
+ 
+    cpberar_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=cp_berar')
+ 
+    ci_nett_gross = movie_val_in_soup('net_box_office.php?cityName=ci')
+   
+    ci_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=ci')
+   
+    nizam_nett_gross = movie_val_in_soup('net_box_office.php?cityName=nizam')
+   
+    nizam_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=nizam')
+ 
+    mysore_nett_gross = movie_val_in_soup('net_box_office.php?cityName=mysore')
+   
+    mysore_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=mysore')
+  
+    tnkerala_nett_gross = movie_val_in_soup('net_box_office.php?cityName=tn_kerla')
+ 
+    tnkerala_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=tn_kerla')
+
+    bihar_nett_gross = movie_val_in_soup('net_box_office.php?cityName=bihar')
+  
+    bihar_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=bihar')
+   
+    wb_nett_gross = movie_val_in_soup('net_box_office.php?cityName=west_bengal')
+
+    wb_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=west_bengal')
+
+    assam_nett_gross = movie_val_in_soup('net_box_office.php?cityName=assam')
+
+    assam_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=assam')
+ 
+    orrisa_nett_gross = movie_val_in_soup('net_box_office.php?cityName=orrisa')
+  
+    orrisa_disti_nett_gross = movie_val_in_soup('india-distributor-share.php?cityName=orrisa')
+
+    rec = [movie_id, mumbai_nett_gross, mumbai_disti_nett_gross, delhi_nett_gross, delhi_disti_nett_gross, 
+           epunjab_nett_gross, epunjab_disti_nett_gross, rajasthan_nett_gross, rajasthan_disti_nett_gross, 
+           cpberar_nett_gross, cpberar_disti_nett_gross, ci_nett_gross, ci_disti_nett_gross, 
+           nizam_nett_gross, nizam_disti_nett_gross, mysore_nett_gross, mysore_disti_nett_gross, 
+           tnkerala_nett_gross, tnkerala_disti_nett_gross, bihar_nett_gross, bihar_disti_nett_gross, 
+           wb_nett_gross, wb_disti_nett_gross, assam_nett_gross, assam_disti_nett_gross, 
+           orrisa_nett_gross, orrisa_disti_nett_gross]
+    regn_cons_lst.append(rec)
+    
+#%% Scratch

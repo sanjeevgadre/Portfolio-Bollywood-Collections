7b3de3f57ba25b13adf5e7378faa304091a69631
Sanjeev Gadre <sanjeev_gadre@hotmail.com>
2020-07-07 17:55:25 +0530

    Extended the correlation analysis

--- corr.py
+++ corr.py
@@ -55,18 +55,22 @@ plt.plot(movie_master['release_year'].unique(), movie_master.groupby('release_ye
 plt.title('Median Budget by Release Year')
 plt.ylabel('Rupees in millions')
 plt.xlabel('Year of Release')
-plt.savefig('./figs/corr/ry-budget.jpg', dpi = 'figure')
+plt.savefig('./figs/corr/ry_budget.jpg', dpi = 'figure')
 plt.show()
 
 #%% Genre v/s Screens - conditioned on Release Year
+chi2_lst = []
 pv_lst = []
 years = movie_master['release_year'].unique()
 for year in years:
     X = movie_master.loc[movie_master['release_year'] == year]['screens']
     Y = movie_master.loc[movie_master['release_year'] == year]['genre']
     crosstab = pd.crosstab(X, Y)
-    _, p_value, _, _ = stats.chi2_contingency(crosstab)
+    chi2, p_value, _, _ = stats.chi2_contingency(crosstab)
+    chi2_lst.append(chi2)
     pv_lst.append(p_value)
+
+print('Average Chi-square statistic: %.4f' % np.mean(chi2_lst))
 
 plt.figure()
 plt.plot(years, pv_lst)
@@ -99,8 +103,34 @@ plt.title('Pearson Correlation Coefficient: Budget v/s Screen')
 plt.grid(axis = 'y')
 plt.savefig('./figs/corr/budget_screen_stratified.jpg', dpi = 'figure')
 plt.show()
+plt.close()
 
 #%% Release Year v/s Screens - conditioned on Budget
+Y = movie_master['screens']
+X = movie_master.loc[:, ['release_year', 'budget']]
+
+# We normalize X and Y as they are on different scales and then add a constant term for intercept. 
+Y = (Y - Y.mean())/Y.std()
+X = (X - X.mean())/X.std()
+X = sm.add_constant(X)
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+plt.figure(figsize = (10, 7))
+plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 10}, fontproperties = 'monospace') 
+plt.axis('off')
+plt.tight_layout()
+plt.savefig('./figs/corr/ry_screens.jpg', dpi = 'figure')
+plt.close()
+
+#%% Continue Here
+
+
+
+
+
+
 
 
 #%% Release Year v/s First Week Revenue
@@ -119,6 +149,7 @@ plt.show()
 
 #%% Genre v/s FWR - conditioned on Release Year
 Y = movie_master.loc[movie_master['release_year'] == 2000]['india_first_week_adj']
+
 X = pd.get_dummies(movie_master.loc[movie_master['release_year'] == 2000]['genre'])
 X = sm.add_constant(X)
 model = sm.OLS(Y/100000,X)

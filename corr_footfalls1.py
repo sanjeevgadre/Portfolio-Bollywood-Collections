03c7974283cea876025104f8f68da1911b34e05e
Sanjeev Gadre <sanjeev_gadre@hotmail.com>
2020-08-24 21:10:44 +0530

    Second version of prediction models - fwr, runlen and ff

--- corr_footfalls1.py
+++ corr_footfalls1.py
@@ -12,11 +12,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels.api as sm
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_val_score
 import pickle
 
 #%% Loading Data
@@ -29,13 +28,13 @@ fwr = movie_master['india-first-week'] * (movie_master['india-nett-gross']/movie
 
 # Getting the run length of films
 df = weekly_master.groupby('movie_id').sum().iloc[:, 3:]
-run_len = df.apply(lambda x: np.count_nonzero(x), axis = 1)
-run_len.reset_index(drop = True, inplace = True)
+run_length = df.apply(lambda x: np.count_nonzero(x), axis = 1)
+run_length.reset_index(drop = True, inplace = True)
 
-#%% Run Length v/s Release Week conditioned on Budget
+#%% Footfalls v/s Release Week conditioned on Budget
 X = movie_master.loc[:, ['release_week', 'budget']]
 X['release_week'] = X['release_week'].astype('float')
-Y = run_len
+Y = movie_master['india-footfalls']
 
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
@@ -43,11 +42,11 @@ Y = (Y - Y.mean())/Y.std()
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length v/s Genre conditioned on Year
+#%% Footfalls v/s Genre conditioned on Year
 X = movie_master.loc[:, ['release_year', 'genre']]
 X['release_year'] = X['release_year'].astype('float')
 X = pd.get_dummies(X)
-Y = run_len
+Y = movie_master['india-footfalls']
 
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
@@ -55,71 +54,93 @@ Y = (Y - Y.mean())/Y.std()
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length v/s First Week Revenue conditioned on Budget, Screens and Year
-X = movie_master.loc[:, ['budget', 'screens', 'release_year']]
-X = pd.concat([X, fwr], axis = 1)
-X.columns = ['budget', 'release_year', 'screens', 'fwr']
-X = X.astype('float')
-Y = run_len
+#%% Footfalls v/s Run Length conditioned on Year, Budget, Screens, Runtime and First Week Revenue
+X = movie_master.loc[:, ['release_year', 'budget', 'screens', 'runtime']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'budget', 'screens', 'runtime', 'fwr', 'runlen']
+Y = movie_master['india-footfalls']
+
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
 
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length v/s Screens conditioned on Budget, Year and First Week Revenue
-X = movie_master.loc[:, ['budget', 'screens', 'release_year']]
-X = pd.concat([X, fwr], axis = 1)
-X.columns = ['budget', 'release_year', 'screens', 'fwr']
-X = X.astype('float')
-Y = run_len
+#%% Footfalls v/s First Week Revenue conditioned on Budget and Screens
+X = movie_master.loc[:, ['release_year', 'budget', 'inf_adj_fct', 'screens']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'budget', 'inf-adj-fct', 'screens', 'fwr', 'runlen']
+Y = movie_master['india-footfalls']
+
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
 
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length v/s Runtime conditioned on Budget and Year
+#%% Footfalls v/s Screens conditioned on Year, Budget, First Week Revenue and Run Length
+X = movie_master.loc[:, ['release_year', 'budget', 'screens']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'budget', 'screens', 'fwr', 'runlen']
+Y = movie_master['india-footfalls']
+
+X = (X - X.mean())/X.std()
+Y = (Y - Y.mean())/Y.std()
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+#%% Footfalls v/s Runtime conditioned on Year, Budget, Screens, 
 X = movie_master.loc[:, ['release_year', 'budget', 'runtime']]
 X['release_year'] = X['release_year'].astype('float')
-Y = run_len
+X = pd.concat([X, run_length], axis = 1)
+X.columns = ['release_year', 'budget', 'runtime', 'runlen']
+Y = movie_master['india-footfalls']
+
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
 
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length v/s Budget, conditioned on Year, Screens, Runtime and First Week Revenue
+#%% Footfalls v/s Budget, conditioned on Year, Screens, First Week Revenue and Run Length
 X = movie_master.loc[:, ['release_year', 'budget', 'screens', 'runtime']]
-X = pd.concat([X, fwr], axis = 1)
-X.columns = ['release_year', 'budget', 'screens', 'runtime', 'fwr']
-X = X.astype('float')
-Y = run_len
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'budget', 'screens', 'runtime', 'fwr', 'runlen']
+Y = movie_master['india-footfalls']
+
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
 
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length v/s Year, conditioned on Screens, Runtime, Budget and First Week Revenue
-X = movie_master.loc[:, ['release_year', 'budget', 'screens', 'runtime']]
-X = pd.concat([X, fwr], axis = 1)
-X.columns = ['release_year', 'budget', 'screens', 'runtime', 'fwr']
-X = X.astype('float')
-Y = run_len
+#%% Footfalls v/s Year conditioned over Genre, Runtime, Screens, First Week Revenue and Run Length
+X = movie_master.loc[:, ['release_year', 'genre', 'screens', 'runtime']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'genre', 'screens', 'runtime', 'fwr', 'runlen']
+X = pd.get_dummies(X)
+Y = movie_master['india-footfalls']
+
 X = (X - X.mean())/X.std()
 Y = (Y - Y.mean())/Y.std()
 
 model = sm.OLS(Y, X).fit()
 print(model.summary())
 
-#%% Run Length Models - Investigating Likely Response Functin
+#%% Footfalls - Features of Likely Predictive Model
+X = movie_master.loc[:, ['release_year', 'genre', 'screens', 'runtime']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'genre', 'screens', 'runtime', 'fwr', 'runlen']
+X = pd.get_dummies(X)
 
-X = movie_master.loc[:, ['release_year', 'screens', 'runtime']]
-X = pd.concat([X, fwr], axis = 1)
-X.columns = ['release_year', 'screens', 'runtime', 'fwr']
-X = X.astype('float')
-Y = run_len
+Y = movie_master['india-footfalls']
 
 # Should not standardize when doing heteroscedacidy analysis as log transformations can lead to NANs
 
@@ -130,14 +151,28 @@ Y_hat = model.predict(X)
 studentized_residuals = model.get_influence().resid_studentized_internal
 
 plt.figure()
-Y_hat = Y_hat
+Y_hat = Y_hat/1000000
 plt.scatter(Y_hat, studentized_residuals)
 plt.grid(True, which = 'major', axis = 'y')
 plt.ylabel('Studentized Residuals')
-plt.xlabel('Predicted Run Length in Weeks')
+plt.xlabel('Predicted Footfalls')
 plt.show()
 
-#--> Clearly there is no heteroscedacity. 
+#--> Clearly there is heteroscedacity. To eliminate heteroscedacity, we try fitting a model using the log values of Y
+Y = np.log(Y)
+model = sm.OLS(Y, X).fit()
+Y_hat = model.predict(X)
+studentized_residuals = model.get_influence().resid_studentized_internal
+
+plt.figure()
+plt.scatter(Y_hat, studentized_residuals)
+plt.grid(True, which = 'major', axis = 'y')
+plt.ylabel('Studentized Residuals')
+plt.xlabel('Predicted Log Value of Footfalls')
+plt.show()
+
+#--> Does eliminate heteroscedacity. Should use log values of Y for further investigation and modelling
+
 
 ## Investigating likely non-linear response function - residuals v/s predicted values
 
@@ -145,18 +180,23 @@ residuals = Y_hat - Y
 plt.figure(figsize = (15, 5))
 sns.set(style="whitegrid")
 axs = sns.residplot(Y_hat, residuals, lowess = True)
-axs.set_xlabel('Predicted Run Length in Weeks')
+axs.set_xlabel('Predicted Log Values of Footfalls')
 axs.set_ylabel('Residuals')
 plt.show()
 
-#--> Perhaps a linear response function
+#--> Perhaps a non-linear response function
 
-#%% Run Length Prediction Models - Tree Based
-X = movie_master.loc[:, ['release_year', 'budget', 'screens', 'runtime']]
-X = pd.concat([X, fwr], axis = 1)
-X.columns = ['release_year', 'budget', 'screens', 'runtime', 'fwr']
-X = X.astype('float')
-Y = run_len
+
+#%% Footfalls Prediction Models - Tree Based
+X = movie_master.loc[:, ['release_year', 'genre', 'screens', 'runtime']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['release_year', 'genre', 'screens', 'runtime', 'fwr', 'runlen']
+
+# Encoding Genre
+X = pd.get_dummies(X)
+
+Y = movie_master['india-footfalls']
 
 rf_est = RandomForestRegressor(random_state = 1970)
 gb_est = GradientBoostingRegressor(random_state = 1970)
@@ -181,16 +221,9 @@ gb_mod = GridSearchCV(gb_est, param_grid = gb_param_grid,
 print('The best fit Gradient Boosted Tree Regressor reports a cross-validated MAE of %.4f' % -gb_mod.best_score_)
 print('The parameters for the best fit model are %s' % gb_mod.best_params_)
 
-# Run Length Prediction Model - Linear
+## Boosted Tree Ensemble delivers best results (lower MAE)
 
-lr_est = LinearRegression()
-lr_mod = cross_val_score(lr_est, X, Y, scoring = 'neg_mean_absolute_error')
-
-print('The Linear Regression Model reports a cross validated MAE of %.4f' % -lr_mod.mean())
-
-## Gradient Boosted Ensemble delivers best results (lower MAE)
-
-#%% Prediction Model Performance
+#%% Footfalls Prediction Model Performance
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state = 1970)
 
@@ -201,10 +234,6 @@ print('The R^2 for the model using test data: %.4f' % gb_mod_best.score(X_test, 
 
 Y_test_hat = gb_mod_best.predict(X_test)
 
-for i in range(len(Y_test_hat)):
-    if Y_test_hat[i] < 1: Y_test_hat[i] = 1
-    if Y_test_hat[i] > 11: Y_test_hat[i] = 11
-
 err_mae = np.abs(Y_test - Y_test_hat)/Y_test
 
 intervals = np.arange(0.25, 0.56, 0.1)
@@ -214,5 +243,6 @@ for interval in intervals:
     print('Percentage of estimates for test set that are off by less than %.0f%% from true value: %.2f' % (100*interval, cnt))
 
 #%% Saving the best fit ensemble parameters
-with open('./runlen_best_param1.pkl', 'w+b') as handle:
+with open('./ff_best_param1.pkl', 'w+b') as handle:
     pickle.dump(gb_mod.best_params_, handle)
+    
---
+++ corr_totrev1.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 17:22:06 2020
+
+@author: sanjeev
+"""
+
+#%% Libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+import pickle
+
+#%% Loading Data
+movie_master = pd.read_pickle('./data/movie_master_en.pkl')
+cpi_master = pd.read_csv('./data/CPI.csv')
+weekly_master = pd.read_hdf('./data/weekly_master.h5')
+
+# Adjusting the first week revenue to account for entertainment and service tax
+fwr = movie_master['india-first-week'] * (movie_master['india-nett-gross']/movie_master['india-total-gross'])
+
+# Getting the run length of films
+df = weekly_master.groupby('movie_id').sum().iloc[:, 3:]
+run_length = df.apply(lambda x: np.count_nonzero(x), axis = 1)
+run_length.reset_index(drop = True, inplace = True)
+
+#%% Total Nett Revenue v/s Release Week conditioned on Budget
+X = movie_master.loc[:, ['release_week', 'budget']]
+X['release_week'] = X['release_week'].astype('float')
+Y = movie_master['india-nett-gross']
+
+X = (X - X.mean())/X.std()
+Y = (Y - Y.mean())/Y.std()
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+#%% Total Nett Revenue v/s Genre conditioned on Year
+X = movie_master.loc[:, ['release_year', 'genre']]
+X['release_year'] = X['release_year'].astype('float')
+X = pd.get_dummies(X)
+Y = movie_master['india-nett-gross']
+
+X = (X - X.mean())/X.std()
+Y = (Y - Y.mean())/Y.std()
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+#%% Total Nett Revenue v/s Footfalls, conditioned on Year, Budget, First Week Revenue and Run Length
+pvalue_lst = []
+years = movie_master['release_year'].unique()
+for year in years:
+    X = movie_master.loc[movie_master['release_year'] == year, ['budget', 'india-footfalls']]
+    indx = movie_master.loc[movie_master['release_year'] == year].index
+    X = pd.concat([X, fwr[indx], run_length[indx], ], axis = 1)
+    X.columns = ['budget', 'footfalls', 'fwr', 'run_len']
+    Y = movie_master['india-nett-gross'][indx]
+    X = (X - X.mean())/X.std()
+    Y = (Y - Y.mean())/Y.std()
+    model = sm.OLS(Y, X).fit()
+    pvalue_lst.append(model.pvalues['footfalls'])
+
+plt.figure()
+plt.scatter(years, pvalue_lst)
+plt.xlabel('Year of Release')
+plt.axhline(y = 0.05, color='r', linestyle='--')
+plt.title('p-values for Regression Coefficient: Total Nett Revenue v/s Footfalls')
+plt.grid(axis = 'y')
+plt.savefig('./figs/tr/03_COR.jpg', dpi = 'figure')
+plt.show()
+plt.close()
+
+
+#%% Total Nett Revenue v/s Run Length conditioned on Year, Screens and First Week Revenue
+pvalue_lst = []
+years = movie_master['release_year'].unique()
+for year in years:
+    X = movie_master.loc[movie_master['release_year'] == year, ['screens']]
+    indx = movie_master.loc[movie_master['release_year'] == year].index
+    X = pd.concat([X, fwr[indx], run_length[indx]], axis = 1)
+    X.columns = ['screens', 'fwr', 'run_len']
+    Y = movie_master['india-nett-gross'][indx]
+    X = (X - X.mean())/X.std()
+    Y = (Y - Y.mean())/Y.std()
+    model = sm.OLS(Y, X).fit()
+    pvalue_lst.append(model.pvalues['run_len'])
+
+plt.figure()
+plt.scatter(years, pvalue_lst)
+plt.xlabel('Year of Release')
+plt.axhline(y = 0.05, color='r', linestyle='--')
+plt.title('p-values for Regression Coefficient: Total Nett Revenue v/s Run Length')
+plt.grid(axis = 'y')
+plt.savefig('./figs/tr/04_COR.jpg', dpi = 'figure')
+plt.show()
+plt.close()
+
+#%% Total Nett Revenue v/s First Week Revenue conditioned on Budget and Screens
+X = movie_master.loc[:, ['budget', 'screens']]
+X = pd.concat([X, fwr], axis = 1)
+X.columns = ['budget', 'screens', 'fwr']
+Y = movie_master['india-nett-gross']
+X = (X - X.mean())/X.std()
+Y = (Y - Y.mean())/Y.std()
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+#%% Total Nett Revenue v/s Screens conditioned on Budget
+X = movie_master.loc[:, ['budget', 'screens']]
+Y = movie_master['india-nett-gross']
+X = (X - X.mean())/X.std()
+Y = (Y - Y.mean())/Y.std()
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+#%% Total Nett Revenue v/s Runtime conditioned on Budget and Year
+pvalue_lst = []
+years = movie_master['release_year'].unique()
+for year in years:
+    X = movie_master.loc[movie_master['release_year'] == year, ['budget', 'runtime']]
+    indx = movie_master.loc[movie_master['release_year'] == year].index
+    Y = movie_master['india-nett-gross'][indx]
+    X = (X - X.mean())/X.std()
+    Y = (Y - Y.mean())/Y.std()
+    model = sm.OLS(Y, X).fit()
+    pvalue_lst.append(model.pvalues['runtime'])
+
+plt.figure()
+plt.scatter(years, pvalue_lst)
+plt.xlabel('Year of Release')
+plt.axhline(y = 0.05, color='r', linestyle='--')
+plt.title('p-values for Regression Coefficient: Total Nett Revenue v/s Runtime')
+plt.grid(axis = 'y')
+plt.savefig('./figs/tr/07_COR.jpg', dpi = 'figure')
+plt.show()
+plt.close()
+
+#%% Total Nett Revenue v/s Budget, conditioned on Year
+years = movie_master['release_year'].unique()
+corr_lst = []
+for year in years:
+    X = movie_master.loc[movie_master['release_year'] == year, 'budget']
+    indx = movie_master.loc[movie_master['release_year'] == year].index
+    Y = movie_master['india-nett-gross'][indx]
+    corr = X.corr(Y, method = 'spearman')
+    corr_lst.append(corr)
+    
+print('Average Spearman Correlation Coeff: %.4f' % np.mean(corr_lst))
+
+plt.figure()
+plt.scatter(years, corr_lst)
+plt.ylim(0, 1)
+plt.xlabel('Year of Release')
+plt.axhline(y = np.mean(corr_lst), color='r', linestyle='-')
+plt.axhline(y = 0.3, color = 'b', linestyle='--')
+plt.title('Spearman Correlation: Total Nett Revenue v/s Budget')
+plt.grid(axis = 'y')
+plt.savefig('./figs/tr/08_COR.jpg', dpi = 'figure')
+plt.show()
+plt.close()
+
+#%% Total Nett Revenue v/s Year, conditioned on Inflation
+X = movie_master['release_year']
+Y = movie_master['india-nett-gross'] * movie_master['inf_adj_fct']
+corr = X.corr(Y, method = 'spearman')
+print('Total Effect of Y on T, adjusted for inflation, : %.4f' % corr)
+
+X = movie_master['budget']
+Y = movie_master['india-nett-gross']
+corr = X.corr(Y, method = 'spearman')
+print('Total Effect of B on E : %.4f' % corr)
+
+## Total Nett Revenue v/s Year
+X = movie_master['release_year']
+Y = movie_master['india-nett-gross']
+corr = X.corr(Y, method = 'spearman')
+print('Total Effect of Y on T : %.4f' % corr)
+
+#%% Total Nett Revenue - Features of Likely Predictive Model
+X = movie_master.loc[:, ['budget', 'india-footfalls', 'screens']]
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['budget', 'india-footfalls', 'screens', 'fwr', 'runlen']
+Y = movie_master['india-nett-gross']
+
+
+# Should not standardize when doing heteroscedacidy analysis as log transformations can lead to NANs
+
+## Investigating heteroscedacity - studentized residuals v/s predicted values
+X = sm.add_constant(X)
+model = sm.OLS(Y, X).fit()
+Y_hat = model.predict(X)
+studentized_residuals = model.get_influence().resid_studentized_internal
+
+plt.figure()
+Y_hat = Y_hat/1000000
+plt.scatter(Y_hat, studentized_residuals)
+plt.grid(True, which = 'major', axis = 'y')
+plt.ylabel('Studentized Residuals')
+plt.xlabel('Predicted Total Nett Revenue')
+plt.show()
+
+#--> Clearly there is heteroscedacity. To eliminate heteroscedacity, we try fitting a model using the log values of Y
+Y = np.log(Y)
+model = sm.OLS(Y, X).fit()
+Y_hat = model.predict(X)
+studentized_residuals = model.get_influence().resid_studentized_internal
+
+plt.figure()
+plt.scatter(Y_hat, studentized_residuals)
+plt.grid(True, which = 'major', axis = 'y')
+plt.ylabel('Studentized Residuals')
+plt.xlabel('Predicted Log Value of Total Nett Revenue')
+plt.show()
+
+#--> Does eliminate heteroscedacity. Should use log values of Y for further investigation and modelling
+
+
+## Investigating likely non-linear response function - residuals v/s predicted values
+
+residuals = Y_hat - Y
+plt.figure(figsize = (15, 5))
+sns.set(style="whitegrid")
+axs = sns.residplot(Y_hat, residuals, lowess = True)
+axs.set_xlabel('Predicted Log Values of Total Nett Revenue')
+axs.set_ylabel('Residuals')
+plt.show()
+
+#--> Likely non-linear response function
+
+
+#%% Total Nett Revenue Prediction Models - Tree Based
+X = movie_master.loc[:, ['budget', 'india-footfalls', 'screens']]
+X = pd.concat([X, fwr, run_length], axis = 1)
+X.columns = ['budget', 'india-footfalls', 'screens', 'fwr', 'runlen']
+Y = movie_master['india-nett-gross']
+
+rf_est = RandomForestRegressor(random_state = 1970)
+gb_est = GradientBoostingRegressor(random_state = 1970)
+
+## Using MAE as evaluation metric
+rf_param_grid = {'n_estimators' : [100, 500, 2500], 'criterion' : ['mse', 'mae'],
+                 'max_depth' : [1, 2, 4], 'max_features' : [0.33, 0.66, 1]}
+rf_mod = GridSearchCV(rf_est, param_grid = rf_param_grid, 
+                      scoring = 'neg_mean_absolute_error', n_jobs = -1,
+                      error_score = 'raise').fit(X, Y)
+
+print('The best fit Random Forest Regressor reports a cross-validated MAE of %.0f' % -rf_mod.best_score_)
+print('The parameters for the best fit model are %s' % rf_mod.best_params_)
+
+gb_param_grid = {'n_estimators' : [100, 500, 2500], 'learning_rate' : [0.01, 0.001], 
+                 'criterion' : ['mse', 'mae'], 'max_depth' : [1, 2, 4], 
+                 'max_features' : [0.33, 0.66, 1]}
+gb_mod = GridSearchCV(gb_est, param_grid = gb_param_grid, 
+                      scoring = 'neg_mean_absolute_error', n_jobs = -1, 
+                      error_score = 'raise').fit(X, Y)
+
+print('The best fit Gradient Boosted Tree Regressor reports a cross-validated MAE of %.0f' % -gb_mod.best_score_)
+print('The parameters for the best fit model are %s' % gb_mod.best_params_)
+
+## Boosted Tree Ensemble delivers best results (lower MAE)
+
+#%% Total Nett Revenue Prediction Model Performance
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state = 1970)
+
+gb_est_best = GradientBoostingRegressor(random_state = 1970).set_params(**gb_mod.best_params_)
+gb_mod_best = gb_est_best.fit(X_train, Y_train)
+print('The R^2 for the model using test data: %.4f' % gb_mod_best.score(X_test, Y_test))
+
+Y_test_hat = gb_mod_best.predict(X_test)
+
+err_mae = np.abs(Y_test - Y_test_hat)/Y_test
+
+intervals = np.arange(0.25, 0.56, 0.1)
+for interval in intervals:
+    cnt = len([x for x in err_mae if x < interval])
+    cnt = 100*cnt/len(err_mae)
+    print('Percentage of estimates for test set that are off by less than %.0f%% from true value: %.2f' % (100*interval, cnt))
+    
+#%% Saving the best fit ensemble parameters
+with open('./totrev_best_param.pkl', 'w+b') as handle:
+    pickle.dump(gb_mod.best_params_, handle)
